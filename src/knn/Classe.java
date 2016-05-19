eb6a8363d16ff1be9d4a85ffc7c74ec647c56daa
marcocmm <marco.cmm@gmail.com>
2016-05-19 19:29:52 -0300

    parseInt

--- src/knn/Classe.java
+++ src/knn/Classe.java
@@ -73,5 +73,36 @@ public enum Classe {
                 return 0;
         }
     }
-    
+
+    public static Classe parseInt(int index) {
+        switch (index) {
+            case 1:
+                return JANEIRO;
+            case 2:
+                return FEVEREIRO;
+            case 3:
+                return MARÇO;
+            case 4:
+                return ABRIL;
+            case 5:
+                return MAIO;
+            case 6:
+                return JUNHO;
+            case 7:
+                return JULHO;
+            case 8:
+                return AGOSTO;
+            case 9:
+                return SETEMBRO;
+            case 10:
+                return OUTUBRO;
+            case 11:
+                return NOVEMBRO;
+            case 12:
+                return DEZEMBRO;
+            default:
+                return null;
+        }
+
+    }
 }
