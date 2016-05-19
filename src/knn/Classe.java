4b688d5c031fcdc876c1d06a667800fbc4532694
marcocmm <marco.cmm@gmail.com>
2016-05-19 19:17:53 -0300

    toInt

--- src/knn/Classe.java
+++ src/knn/Classe.java
@@ -42,4 +42,36 @@ public enum Classe {
                 return null;
         }
     }
+
+    public static int toInt(Classe classe) {
+        switch (classe) {
+            case JANEIRO:
+                return 1;
+            case FEVEREIRO:
+                return 2;
+            case MARÇO:
+                return 3;
+            case ABRIL:
+                return 4;
+            case MAIO:
+                return 5;
+            case JUNHO:
+                return 6;
+            case JULHO:
+                return 7;
+            case AGOSTO:
+                return 8;
+            case SETEMBRO:
+                return 9;
+            case OUTUBRO:
+                return 10;
+            case NOVEMBRO:
+                return 11;
+            case DEZEMBRO:
+                return 12;
+            default:
+                return 0;
+        }
+    }
+    
 }
