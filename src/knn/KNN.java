78d1e472a94cc43389e30c4c55e21d72f7384c2b
Rômulo Manciola Meloca <rmeloca@gmail.com>
2016-05-19 17:48:38 -0300

    a

--- src/knn/KNN.java
+++ src/knn/KNN.java
@@ -5,16 +5,13 @@
  */
 package knn;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
